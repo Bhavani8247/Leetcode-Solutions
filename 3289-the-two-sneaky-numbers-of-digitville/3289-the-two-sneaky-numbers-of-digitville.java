f5ad6f4c0bd158ffdf277dca878befce1064073e
Bhavani8247 <147934441+Bhavani8247@users.noreply.github.com>
2025-10-31 10:40:46 +0530

    Time: 3 ms (36.86%), Space: 44.8 MB (59.98%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                duplicates.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+
+        // Convert List<Integer> to int[]
+        int[] result = new int[duplicates.size()];
+        for (int i = 0; i < duplicates.size(); i++) {
+            result[i] = duplicates.get(i);
+        }
+
+        return result;
+    }
+}
